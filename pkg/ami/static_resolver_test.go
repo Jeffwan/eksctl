e7c8b1a4add145934c4eba1be65c2eae9b686eaa
Jiaxin Shan <seedjeffwan@gmail.com>
2019-09-22 16:29:39 -0700

    Add support for EC2 instance of G4 type

--- pkg/ami/static_resolver_test.go
+++ pkg/ami/static_resolver_test.go
@@ -96,6 +96,14 @@ var _ = Describe("AMI Static Resolution", func() {
 			ExpectedAMI:  "ami-04668c090ff8c1f50",
 			ExpectError:  false,
 		}),
+                Entry("with gpu (g4dn) instance and eu-west-1", ResolveCase{
+                        Region:       "eu-west-1",
+                        Version:      "1.13",
+                        InstanceType: "g4dn.4xlarge",
+                        ImageFamily:  "AmazonLinux2",
+                        ExpectedAMI:  "ami-04668c090ff8c1f50",
+                        ExpectError:  false,
+                }),
 		Entry("with gpu (p3) instance and non-existent region", ResolveCase{
 			Region:       "eu-east-1",
 			Version:      "1.12",
